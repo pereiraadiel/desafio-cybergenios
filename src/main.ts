e6566264f3d8e5a60bcbbea67cb2479e19a72acd
Adiel <adielp16@gmail.com>
2022-07-14 21:29:05 -0300

    :wrench: config prisma client

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'src/infra/app.module';
+import { AppModule } from '@infra/app.module';
+import { PrismaService } from '@infra/config/PrismaService';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 bootstrap();
