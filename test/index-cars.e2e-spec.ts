36967cc62011019b5f2c2678eaf204f910460731
Adiel <adielp16@gmail.com>
2022-07-16 17:29:49 -0300

    :white_check_mark: add e2e tests for index-cars route

---
+++ test/index-cars.e2e-spec.ts
@@ -0,0 +1,46 @@
+import { AppModule } from '@infra/app.module';
+import { INestApplication } from '@nestjs/common';
+import { TestingModule, Test } from '@nestjs/testing';
+import * as request from 'supertest';
+
+describe('get-car (e2e)', () => {
+  let app: INestApplication;
+  let accessToken = '';
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    await request(app.getHttpServer())
+      .post('/auth')
+      .send({ email: 'admin@cybergenios.com.br', password: 'cybergenios' })
+      .then((response) => {
+        accessToken = response.body.accessToken;
+      });
+  });
+
+  it('/cars (GET) should not be able to get cars without authentication', () => {
+    return request(app.getHttpServer())
+      .get('/cars')
+      .then((response) => {
+        expect(response.statusCode).toBe(401);
+      });
+  });
+
+  it('/cars (GET) should be able to get cars', () => {
+    return request(app.getHttpServer())
+      .get('/cars')
+      .set({ Authorization: `Bearer ${accessToken}` })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
