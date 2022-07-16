af2830672ba29996ee9d40f54760cab67182b4a1
Adiel <adielp16@gmail.com>
2022-07-16 14:15:41 -0300

    :white_check_mark: add tests to create-customer usecase/controller

--- src/infra/controllers/users/create-customer/create-customer.controller.spec.ts
+++ src/infra/controllers/users/create-customer/create-customer.controller.spec.ts
@@ -5,17 +5,40 @@ import { CreateCustomerController } from './create-customer.controller';
 
 describe('CreateCustomerController', () => {
   let controller: CreateCustomerController;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CreateCustomerController],
       providers: [PrismaService, CreateCustomerService],
     }).compile();
+    prisma = module.get<PrismaService>(PrismaService);
+    prisma.user.findUnique = jest.fn().mockReturnValueOnce(null);
+    prisma.user.create = jest.fn().mockReturnValueOnce({
+      id: '7ad6e84a-98a3-4b2e-916d-962688f55646',
+      address: 'rua teste',
+      cpf: '1232312',
+      email: 'test@test.com',
+      password: '$2y$10$vfknSYpnA2T52UFOua5sFONesek/CO2JwmpLvTAS227NjN.H2Z4ne',
+      name: 'test',
+      phone: '34123123123',
+      role: 'customer',
+    });
 
     controller = module.get<CreateCustomerController>(CreateCustomerController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should returns user with password hash', async () => {
+    const user = await controller.handle({
+      address: 'rua teste',
+      cpf: '1232312',
+      email: 'test@test.com',
+      password: 'test123',
+      name: 'test',
+      phone: '34123123123',
+      role: 'customer',
+    });
+    expect(user.password).toContain('$2y$10$');
+    expect(user.id).toBeDefined();
   });
 });
--- src/usecases/users/create-customer/create-customer.service.spec.ts
+++ src/usecases/users/create-customer/create-customer.service.spec.ts
@@ -1,18 +1,54 @@
+import { HttpException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@infra/config/PrismaService';
 import { CreateCustomerService } from './create-customer.service';
 
 describe('CreateCustomerService', () => {
   let service: CreateCustomerService;
+  let prisma: PrismaService;
+  const mock = {
+    id: '7ad6e84a-98a3-4b2e-916d-962688f55646',
+    address: 'rua teste',
+    cpf: '1232312',
+    email: 'test@test.com',
+    password: '$2y$10$vfknSYpnA2T52UFOua5sFONesek/CO2JwmpLvTAS227NjN.H2Z4ne',
+    name: 'test',
+    phone: '34123123123',
+    role: 'customer',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateCustomerService],
+      providers: [CreateCustomerService, PrismaService],
     }).compile();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    prisma.user.findUnique = jest.fn().mockReturnValueOnce(null);
+    prisma.user.create = jest.fn().mockReturnValueOnce({
+      ...mock,
+      role: 'customer',
+    });
 
     service = module.get<CreateCustomerService>(CreateCustomerService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be able to create user', async () => {
+    const user = await service.execute({
+      ...mock,
+      password: 'test123',
+      role: 'customer',
+    });
+    expect(user.password).toContain('$2y$10$');
+    expect(user.id).toBeDefined();
+  });
+  it('should not be able to create user with already in use cpf or email', async () => {
+    prisma.user.findUnique = jest.fn().mockReturnValueOnce(mock);
+    await expect(
+      service.execute({
+        ...mock,
+        password: 'test123',
+        role: 'customer',
+      }),
+    ).rejects.toBeInstanceOf(HttpException);
   });
 });
