e3e124e60005f1fbf1edae1407f4383d605c7f78
Adiel <adielp16@gmail.com>
2022-07-16 14:37:14 -0300

    :white_check_mark: add tests to assign-admin usecase

--- src/usecases/users/assign-admin/assign-admin.service.spec.ts
+++ src/usecases/users/assign-admin/assign-admin.service.spec.ts
@@ -1,18 +1,46 @@
+import { HttpException } from '@nestjs/common';
+import { PrismaService } from '@infra/config/PrismaService';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssignAdminService } from './assign-admin.service';
 
 describe('AssignAdminService', () => {
   let service: AssignAdminService;
+  let prisma: PrismaService;
+  const mock = {
+    id: '7ad6e84a-98a3-4b2e-916d-962688f55646',
+    address: 'rua teste',
+    cpf: '1232312',
+    email: 'test@test.com',
+    password: '$2y$10$vfknSYpnA2T52UFOua5sFONesek/CO2JwmpLvTAS227NjN.H2Z4ne',
+    name: 'test',
+    phone: '34123123123',
+    role: 'customer',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssignAdminService],
+      providers: [AssignAdminService, PrismaService],
     }).compile();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    prisma.user.findUnique = jest.fn().mockReturnValueOnce(mock);
+    prisma.user.update = jest.fn().mockReturnValueOnce({
+      ...mock,
+      role: 'admin',
+    });
 
     service = module.get<AssignAdminService>(AssignAdminService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should not be able to assign admin role if user does not exist', async () => {
+    prisma.user.findUnique = jest.fn().mockReturnValueOnce(null);
+    await expect(service.execute(mock.id)).rejects.toBeInstanceOf(
+      HttpException,
+    );
+  });
+
+  it('should be able to assign admin role', async () => {
+    const user = await service.execute(mock.id);
+    expect(user.role).toBe('admin');
   });
 });
