2991f6b5f22315905def7746834219b7bdded02f
Adiel <adielp16@gmail.com>
2022-07-16 15:16:30 -0300

    :white_check_mark: add tests to get-car usecase

--- src/usecases/cars/get-car/get-car.service.spec.ts
+++ src/usecases/cars/get-car/get-car.service.spec.ts
@@ -1,18 +1,39 @@
+import { HttpException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@infra/config/PrismaService';
 import { GetCarService } from './get-car.service';
 
 describe('GetCarService', () => {
   let service: GetCarService;
+  let prisma: PrismaService;
+  const mock = {
+    id: '7ad6e84a-98a3-4b2e-916d-962688f55646',
+    name: 'Golf',
+    model: 'GTI',
+    brand: 'VolksWagen',
+    year: '2014',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GetCarService],
+      providers: [GetCarService, PrismaService],
     }).compile();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    prisma.car.findUnique = jest.fn().mockReturnValueOnce(mock);
 
     service = module.get<GetCarService>(GetCarService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be able to get car', async () => {
+    const user = await service.execute(mock.id);
+    expect(user).toMatchObject(mock);
+  });
+
+  it('should not be able to get car thats does not exist', async () => {
+    prisma.car.findUnique = jest.fn().mockReturnValueOnce(null);
+    await expect(service.execute(mock.id)).rejects.toBeInstanceOf(
+      HttpException,
+    );
   });
 });
